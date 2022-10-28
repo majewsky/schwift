5fd42a265399623a5c1793d0a18cd7369c755c82
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-10-28 13:09:11 +0200

    Cleanup container and eact to Ctrl+C

--- testing/start-saio.sh
+++ testing/start-saio.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 if docker inspect schwift-testing &>/dev/null; then
   echo 'Already running.'
 else
-  exec docker run --name schwift-testing -P -t dockerswiftaio/docker-swift:2.27.0
+  exec docker run --name schwift-testing -P -it --rm dockerswiftaio/docker-swift:2.27.0
 fi
