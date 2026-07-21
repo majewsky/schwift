8e41472447315290b2748709e09b27372adc2b7c
Stefan Majewsky <majewsky@gmx.net>
2026-07-21 14:34:57 +0200

    convert copyright headers to SPDX format

--- tests/pkg.go
+++ tests/pkg.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2021 Stefan Majewsky <majewsky@gmx.net>
+// SPDX-License-Identifier: Apache-2.0
+
 package tests
 
 // This empty source file is required to make `make test` work.
